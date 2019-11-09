5985d87d9808896bb29c232b53d11e3ddc70b884
Paulo Soto <paulocsoto@outlook.com>
2019-11-09 04:45:53 -0500

    Added code to generate a new JSON with merged data from Open Brewery DB and Yelp Ratings and display it on the webpage.

--- BreweryRatings/Pages/Index.cshtml.cs
+++ BreweryRatings/Pages/Index.cshtml.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using BreweryNamespace;
 using System.Net;
+using System.Net.Http;
+using System.IO;
+using Yelp;
+using BusinessNamespace;
 
 namespace BreweryRatings.Pages
 {
@@ -19,14 +23,90 @@ namespace BreweryRatings.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+//        public void OnGet()
+        public JsonResult OnGet()
+
         {
+            IDictionary<string, Business> allBusinesses = new Dictionary<string, Business>();
+
+            Task<string> t = Http_Get();
+
+            var yelpRatingsString = t.Result;
+
+            YelpRating yelpRating = YelpRating.FromJson(yelpRatingsString);
+
+            ViewData["YelpRatings"] = yelpRating;
+
+            Business[] businesses = yelpRating.Businesses;
+
+            foreach(Business business in businesses)
+            {
+                allBusinesses.Add(business.Name, business);
+            }
+
             using (var webClient = new WebClient())
             {
+                List<Brewery> breweriesWithRatings = new List<Brewery>();
+                List<BusinessWithRating> businessesWithRatings = new List<BusinessWithRating>();
                 string jsonString = webClient.DownloadString("https://api.openbrewerydb.org/breweries?by_city=cincinnati");
                 var brewery = Brewery.FromJson(jsonString);
-                ViewData["Breweries"] = brewery;
+
+                foreach(Brewery brewbar in brewery)
+                {
+                    if (allBusinesses.ContainsKey(brewbar.Name))
+                    {
+                        var addBiz = new BusinessWithRating();
+                        addBiz.Id = brewbar.Id;
+                        addBiz.Name = brewbar.Name;
+//                        addBiz.BreweryType = brewbar.BreweryType;
+                        addBiz.Street = brewbar.Street;
+                        addBiz.City = brewbar.City;
+                        addBiz.Latitude = brewbar.Latitude;
+                        addBiz.Longitude = brewbar.Longitude;
+                        //                        addBiz.Rating = allBusinesses.Ratings
+
+                        foreach (Business business in businesses)
+                        {
+                            if (business.Name == brewbar.Name)
+                            {
+                                addBiz.Rating = business.Rating;
+                                break;
+                            }
+                        }
+
+                        businessesWithRatings.Add(addBiz);
+
+//                        breweriesWithRatings.Add(addBiz);
+                    }
+                }
+
+                return new JsonResult(businessesWithRatings);
+
+//                return new JsonResult(breweriesWithRatings);
+
+//                ViewData["Breweries"] = breweriesWithRatings;
             }
+        }
+
+        static async Task<string> Http_Get()
+        {
+            var targetURL = "https://api.yelp.com/v3/businesses/search?location=cincinnati&categories=breweries";
+
+            String myYelpKey = System.IO.File.ReadAllText("YelpAPIKey.txt");
+
+            HttpClientHandler handler = new HttpClientHandler();
+
+            HttpClient client = new HttpClient(handler);
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", myYelpKey);
+
+            HttpResponseMessage response = await client.GetAsync(targetURL);
+
+            HttpContent content = response.Content;
+
+            string result = await content.ReadAsStringAsync();
+
+            return result;
         }
     }
 }
