1cc1a822405455459c96f2e800c0fce452367969
Paulo Soto <paulocsoto@outlook.com>
2019-11-09 00:29:24 -0500

    Added Http_Get code to Get Yelp Ratings for Breweries in Cincinnati

---
+++ BreweryRatings/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using BreweryNamespace;
+using System.Net;
+
+namespace BreweryRatings.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            using (var webClient = new WebClient())
+            {
+                string jsonString = webClient.DownloadString("https://api.openbrewerydb.org/breweries?by_city=cincinnati");
+                var brewery = Brewery.FromJson(jsonString);
+                ViewData["Breweries"] = brewery;
+            }
+        }
+    }
+}
