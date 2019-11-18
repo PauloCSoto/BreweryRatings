fb3a60eb3b2b3f7c893c3845aad0c5424c60c016
Paulo Soto <paulocsoto@outlook.com>
2019-11-18 16:23:17 -0500

    Fixed jQuery and Site.css.  Added Star ratings (instead of numeric ratings).  Applied bootstrap styles to table.

--- BreweryRatings/wwwroot/js/site.js
+++ BreweryRatings/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+$.fn.stars = function () {
+return this.each(function (_i, e) {$(e).html($('<span/>').width($(e).text() * 16)); });
+};
+
+$('.stars').stars();
