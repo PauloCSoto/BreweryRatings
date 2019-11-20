6ff9b760c758dbff6680b6efa5d92d640f335c14
sharangnimbalkar <30493774+sharangnimbalkar@users.noreply.github.com>
2019-11-20 03:50:34 -0500

    Following technical changes made in the code: • To make the project look more organized, a model folder was created to hold all the classes. • For all the .cs files, the access modifiers of variables were changed to make sure that the data is encapsulated. • A sample background image was added to the index page for better UI experience. List three items you learned from the code review: • First and most important, importance of documentation in any project. • Learnt how to create a feature branch. • How to review a code and via GitHub by creating a branch and a pull request. Was the project available in GitHub on time? Yes Does the project compile? Yes Does the project have adequate documentation? Yes

--- BreweryRatings/Pages/Index.cshtml.cs
+++ BreweryRatings/Pages/Index.cshtml.cs
@@ -14,7 +14,6 @@ namespace BreweryRatings.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
