0faa333ab83180cae57c7f1a8560c7a454a9f373
schaepse <schaepse@mail.uc.edu>
2019-11-29 08:50:52 -0500

    Create BusinessLicenseOwners.cs

---
+++ BreweryRatings/BusinessLicenseOwners.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BreweryRatings
+{
+    public class BusinessLicenseOwners
+    {
+        public long AccountNumber { get; set; }
+        public string DoingBusinessAsName { get; set; }
+        public string OwnerFirstName { get; set; }
+        public string OwnerLastName { get; set; }
+        public string OwnerTitle { get; set; }
+        public long LicenseNumber { get; set; }
+        public string State { get; set; }
+        public string BusinessActivity { get; set; }
+        public string City { get; set; }
+    }
+}
