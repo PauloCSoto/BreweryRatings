7757912d7b0e0e0048e324232ffc88f3c58dcd77
Morgan Miller <momiller88@gmail.com>
2019-12-02 18:27:53 -0500

    Added page to consume other groups json, cleaned up table formatting, added basic Json schema for our output

---
+++ BreweryRatings/Pages/BusinessLicensePage.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using BusinessLicenseOwnerNamespace;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BreweryRatings.Pages
+{
+    public class BusinessLicensePageModel : PageModel
+    {
+        public void OnGet()
+        {
+            List<BusinessLicenseOwnerNamespace.BusinessLicenseOwner> businessLicenseOwners = new List<BusinessLicenseOwnerNamespace.BusinessLicenseOwner>();
+
+            using (WebClient webClient = new WebClient())
+            {
+                string licenseJsonString = webClient.DownloadString("https://licenseowners2019.azurewebsites.net/Privacy");
+                BusinessLicenseOwner[] businessLicenses = BusinessLicenseOwner.FromJson(licenseJsonString);
+
+                foreach (BusinessLicenseOwnerNamespace.BusinessLicenseOwner businessLicense in businessLicenseOwners)
+                {
+                    businessLicenseOwners.Add(new BusinessLicenseOwner
+                    {
+                        AccountNumber = businessLicense.AccountNumber,
+                        DoingBusinessAsName = businessLicense.DoingBusinessAsName,
+                        OwnerFirstName = businessLicense.OwnerFirstName,
+                        OwnerLastName = businessLicense.OwnerLastName,
+                        LicenseNumber = businessLicense.LicenseNumber
+                    });
+                }
+
+                ViewData["BusinessLicenseOwners"] = businessLicenseOwners;
+            }
+
+        }
+    }
+}
